b22771caf7b7a99c7ee1d0028b5dffed4897c4fa
harshaavardhinirajaguru-afk <harshaavardhinirajaguru@gmail.com>
2026-04-20 04:12:27 +0000

    Coded uc5

---
+++ uc5.java
@@ -0,0 +1,13 @@
+public class uc5{
+    public static void main(String[]args){
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+        for(String name : args){
+            if (!first){
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+    }
+}
