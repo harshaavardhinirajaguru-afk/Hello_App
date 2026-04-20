f05a6189dec1087730c35938f49048a13bebfd39
harshaavardhinirajaguru-afk <harshaavardhinirajaguru@gmail.com>
2026-04-20 04:01:03 +0000

    Coded uc4

---
+++ uc4.java
@@ -0,0 +1,12 @@
+public class uc4{
+    public static void main(String[]args){
+        StringBuilder nsameBuilder = new StringBuilder();
+        for (int i=0; i, args.length; i++){
+            nameBuilder.append(args[i]);
+            if(i<args.length - 1){
+                nameBuilder.append(",");
+            }
+            name= nameBuilder.toString();
+        }
+    }
+}
