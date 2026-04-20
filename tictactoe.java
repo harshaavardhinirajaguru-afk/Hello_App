aef0446a4a9db6b5d486aae2d9a96868b693dc61
harshaavardhinirajaguru-afk <harshaavardhinirajaguru@gmail.com>
2026-04-20 03:15:20 +0000

    Coded tictactoe

---
+++ tictactoe.java
@@ -0,0 +1,16 @@
+public class tictactoe(
+    static char [][] board = new char [3][3];
+    
+    public static void main(String [] args){
+        initializeBoard();
+        printBoard();
+    }
+    static void initializeBoard(){
+        static void printBoard(){
+            System.out.println(x: "____________");
+            for(int row = 0; row < 3; row++){
+                
+            }
+        }
+    }
+)
